a393dff8a4819f2d62e0437820c436b307e8b413
Kaylee Buscemi <kaylee.buscemi@gmail.com>
2018-03-15 14:53:28 -0700

    Deploy test

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './NavBar.css'
 import { Navbar, NavItem } from 'react-materialize';
+import {Link} from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <div>
+          <Link to='/portfolio'>Linkortfolio</Link>
           <Navbar brand='Kaylee Buscemi' right>
             <NavItem href='/'>Home</NavItem>
-            <NavItem href='/Portfolio'>Portfolio</NavItem>
+            <NavItem href='/portfolio'>Portfolio</NavItem>
             <NavItem href='/about'>About</NavItem>
           </Navbar>
         </div>
