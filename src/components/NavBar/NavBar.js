4e135857385337c9c303ef2efe7bf77fbe7f8b9a
Kaylee Buscemi <kaylee.buscemi@gmail.com>
2018-03-15 15:17:37 -0700

    nav bar experiement

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,12 +6,36 @@ import {Link} from 'react-router-dom';
 const NavBar = () => {
     return (
         <div>
-          <Link to='/portfolio'>Linkortfolio</Link>
-          <Navbar brand='Kaylee Buscemi' right>
+          {/* <ul className='hide-on-med-and-down'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/portfolio'>Portfolio</Link></li>
+          </ul> */}
+          {/* <Navbar brand='Kaylee Buscemi' right>
             <NavItem href='/'>Home</NavItem>
             <NavItem href='/portfolio'>Portfolio</NavItem>
             <NavItem href='/about'>About</NavItem>
-          </Navbar>
+          </Navbar> */}
+
+          <nav>
+            <div class='nav-wrapper'>
+              <div class='col s12'>
+                <a href='/' class='brand-logo'>Kaylee Buscemi</a>
+              <ul class='right hide-on-med-and-down'>
+                <li>
+                  <a href='/'>Home</a>
+                </li>
+                <li>
+                  <a href='/portfolio'>Portfolio</a>
+                </li>
+                <li>
+                  <a href='/about'>About</a>
+                </li>
+              </ul>
+              </div>
+            </div>
+          </nav>
+
         </div>
 
     )
