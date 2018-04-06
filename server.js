a42931774ab336f47bed1892e7796edfddac9ff1
Kaylee Buscemi <kaylee.buscemi@gmail.com>
2018-04-06 16:24:37 -0700

    favicon/build in server.js

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const favicon = require('serve-favicon');
+
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+
+
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
