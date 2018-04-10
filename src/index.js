77af66e9287d61149dc98462f2bb0ce36e927bef
Kaylee Buscemi <kaylee.buscemi@gmail.com>
2018-04-09 17:18:03 -0700

    dry code

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 // import { BrowserRouter as Router, Route  } from 'react-router-dom'; 
 import { HashRouter as Router, Route  } from 'react-router-dom'; 
-// import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(<Router><Route component={App} /></Router>, document.getElementById('root'));
-// ReactDOM.render(<HashRouter> <App /> </HashRouter>, document.getElementById('root'));
 
 
