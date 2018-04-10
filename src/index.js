766613f29cbfdbeace8248f490fe6476deaa7e65
Kaylee Buscemi <kaylee.buscemi@gmail.com>
2018-04-09 17:14:54 -0700

    hashrouter

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Route  } from 'react-router-dom'; 
+// import { BrowserRouter as Router, Route  } from 'react-router-dom'; 
+import { HashRouter as Router, Route  } from 'react-router-dom'; 
+// import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(<Router><Route component={App} /></Router>, document.getElementById('root'));
+// ReactDOM.render(<HashRouter> <App /> </HashRouter>, document.getElementById('root'));
 
 
