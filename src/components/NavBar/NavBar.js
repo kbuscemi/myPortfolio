4393b5317b54b1cc2cb7568e04249ffc9ada0517
Kaylee Buscemi <kaylee.buscemi@gmail.com>
2018-05-15 14:42:42 -0700

    project links/descriptions

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './NavBar.css'
 import {Link} from 'react-router-dom';
-import MediaQuery from 'react-responsive';
 
 const NavBar = () => {
     return (
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,86 +1,7 @@
 import React from 'react';
 import './Portfolio.css';
-import { MediaBox } from 'react-materialize';
 import NavBar from '../NavBar/NavBar';
 import { Card, CardTitle } from 'react-materialize';
-
-
-// const Portfolio = () => {
-//     return (
-//         <div className='container3'>
-//           <NavBar />
-//             <div className='headingContainer'>
-//                 <div className='heading'>
-//                     <h4>Portfolio</h4>
-//                 </div>
-//             </div>
-//             <div className='cardContainer'>
-//                 <div className='row1'>
-//                     <div className='card1'>
-//                         <p className='title'>Flight&nbsp;Tracker</p>
-//                         <p className='tech'>React • Express • Node.js • Mongoose/MongoDB • JavaScript</p>
-//                         <MediaBox src='https://i.imgur.com/LllyjgM.png' caption="Flight Tracker" width="425" className='flight'/>
-//                     </div>
-//                     <div className='context1'>
-//                         <p>Flight Tracker is an application that allows users to submit and view their upcoming flights. Consuming Google Maps 
-//                            and Google Places API, users are able to look at a map and see which airports they will be flying to and from. 
-//                            <br />
-//                            <br />
-//                            <a href='https://flight-planner.herokuapp.com/' target='blank'>View Online</a> | <a href='https://github.com/kbuscemi/flight-planner' target='blank'>View Source</a> 
-//                         </p>
-//                     </div>
-//                 </div>
-//                 <div className='row2'>
-//                     <div className='card2'>
-//                         <p className='title'>Music&nbsp;DB</p>
-//                         <p className='tech'>Express • Node.js • Mongoose/MongoDB • JavaScript</p>
-//                         <MediaBox src='https://i.imgur.com/1jBXc7C.png' caption="Music DB" width="425"/> 
-//                     </div>
-//                     <div className='context2'>
-//                         <p>
-//                            Acting as the IMDB but for the music industry, Music DB showcases individuals who helped bring a song to life. By integrating 
-//                            Spotify's API into the application, musicians can search Spotify's database to locate their songs. 
-//                            <br />
-//                            <br />
-//                            <a href='http://imusicdb.herokuapp.com' target='blank'>View Online</a> | <a href='https://github.com/kbuscemi/music-db' target='blank'>View Source</a> 
-//                         </p>
-//                     </div>
-//                 </div>
-//                 <div className='row3'>
-//                     <div className='card3'>
-//                         <p className='title'>Read&nbsp;with&nbsp;Me</p>
-//                         <p className='tech'>Ruby • Ruby on Rails • HTML • CSS</p>
-//                         <MediaBox src='https://i.imgur.com/rdWD9Vs.png' caption="Read with Me" width="425"/>
-//                     </div>
-//                     <div className='context3'>
-//                         <p>Read with Me is a full stack Ruby application that was developed so users can create reading lists. Whether users are looking for a collection of books to read during the summer or over the holidays
-//                            they can just add them to their respective reading lists.
-//                            <br />
-//                            <br />
-//                            <a href='https://read-w-mee.herokuapp.com/' target='blank'>View Online</a> | <a href='https://github.com/kbuscemi/Read_With_Me' target='blank'>View Source</a>
-//                         </p>
-//                     </div>
-//                 </div>
-//                 <div className='row4'>
-//                     <div className='card4'>
-//                         <p className='title'>Mancala</p>
-//                         <p className='tech'>JavaScript • HTML • CSS</p>
-//                         <MediaBox src='https://i.imgur.com/WQngbWC.png' caption="Mancala" width="425"/>
-//                     </div>
-//                     <div className='context4'>
-//                         <p>Mancala is a web browser game modeled after the original Mancala game that is said to have been created around 700 AD.
-//                            <br />
-//                            <br />
-//                            <a href='https://kbuscemi.github.io/Mancala/' target='blank'>View Online</a> | <a href='https://github.com/kbuscemi/Mancala' target='blank'>View Source</a>
-//                         </p>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Portfolio;
 
 const Portfolio = () => {
     return (
@@ -98,13 +19,16 @@ const Portfolio = () => {
                                 reveal={
                                     <p className='context'>
                                         Flight Tracker is a platform for users to log and track their upcoming flight itineraries. A Heroku hosted
-                                        site written as a full stack MERN app with Google Maps and Google Places Integration.
+                                        site written as a full stack MERN application with Google Maps and Google Places Integration.
                                         <br/>
                                         <br/>
                                         JavaScript + React.js + Node.js + Express.js
                                         <br/>
                                         + MongoDB
                                         <br/>
+                                        <br/>
+                                        <a href='https://flight-planner.herokuapp.com/' target='blank'><i className="fa fa-external-link"></i></a>
+                                        <a href='https://github.com/kbuscemi/flight-planner' target='blank'><i className="fa fa-github"></i></a>
                                     </p>
                                 }>
                             </Card>
@@ -114,7 +38,16 @@ const Portfolio = () => {
                                 title="Music DB"
                                 reveal={
                                     <p>
-                                        Add Description
+                                        Acting as the IMDB but for the music industry, Music DB showcases individuals who helped bring songs to life.
+                                        Built with Node.js and MongoDB. Features the ability to login using your Facebook account and search
+                                        millions of songs through Spotfiy's API.
+                                        <br/>
+                                        <br/>
+                                        Express.js + Node.js + MongoDB + JavaScript
+                                        <br/>
+                                        <br/>
+                                        <a href='http://imusicdb.herokuapp.com' target='blank'><i className="fa fa-external-link"></i></a>
+                                        <a href='https://github.com/kbuscemi/music-db' target='blank'><i className="fa fa-github"></i></a>
                                     </p>
                                 }>
                             </Card>
@@ -126,7 +59,15 @@ const Portfolio = () => {
                                 title="Read with Me"
                                 reveal={
                                     <p>
-                                        Add Description
+                                        Read with Me is a platform to find books to create and compile reading book lists.
+                                        Features the ability to research book information.
+                                        <br/>
+                                        <br/>
+                                        Ruby + Ruby on Rails
+                                        <br/>
+                                        <br/>
+                                        <a href='https://read-w-mee.herokuapp.com/' target='blank'><i className="fa fa-external-link"></i></a>
+                                        <a href='https://github.com/kbuscemi/Read_With_Me' target='blank'><i className="fa fa-github"></i></a>
                                     </p>
                                 }>
                             </Card>
@@ -141,6 +82,9 @@ const Portfolio = () => {
                                         <br/>
                                         JavaScript + JQuery
                                         <br/>
+                                        <br/>
+                                        <a href='https://kbuscemi.github.io/Mancala/' target='blank'><i className="fa fa-external-link"></i></a>
+                                        <a href='https://kbuscemi.github.io/Mancala/' target='blank'><i className="fa fa-github"></i></a>
                                     </p>
                                 }>
                             </Card>
@@ -148,7 +92,6 @@ const Portfolio = () => {
                 </div>
             </div>
         </div>
-
     )
 }
 
