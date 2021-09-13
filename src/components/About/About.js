f1308ad7d67c7c8bd3af3e804c3a08cd1f537161
kbusccemi <kaylee.buscemi@gmail.com>
2021-09-13 16:36:14 -0500

    restructured the nav, portfolio, and about me pages.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,37 +6,34 @@ const About = () => {
     return (
         <div className='container2'>
           <NavBar />
-            <div className='about'>
-             <div className='border'>
+            {/* <div className='about'> */}
+             <div className='containerWrapper'>
                 <div className='col s6 picture'>
                     <div className='imgPic'>
                     <img src="https://i.imgur.com/imOPJjP.png" alt='myPic' className='myPic' />
                     </div>
                 </div>
                 <div className='col s6 aboutMe'>
-                    <h3>About Me</h3>
-                    <span>As a web developer I strive to build immersive and intuitive web applications. Always seeking out 
-                        opportunities and challenges that are meaningful to me, it is my goal to work on web designs that will 
-                        make an impact.
+                    <h3 className='title'>About Me</h3>
+                    <p className='context'>As a frontend engineer I strive to build immersive and intuitive applications. I am always seeking out 
+                        opportunities and challenges that are meaningful to me both personally and professionally.
                         <br/>
                         <br/>
-                        When I'm not coding, I'm either out in the wilderness or at a cafe somewhere reading a book.
+                        When I'm not coding, I'm either out in nature with my dog or I'm a cafe drinking coffee and reading a book.
                         <br/>
                         <br/>
-                        If you would like to get in touch, feel free to contact me.
-                    </span>
-                        <br/>
-                        <div>
-                        <div className='icons'>
-                            <a href='https://github.com/kbuscemi' target='blank'><i className="fa fa-github github"></i></a>
-                            <a href='https://www.linkedin.com/in/kayleebuscemi/' target='blank'><i className="fa fa-linkedin-square linkedin"></i></a>
-                            <span className='email'>email: kaylee.buscemi@gmail.com</span>
-                        </div>
+                        If you would like to get in touch, feel free to reach out.
+                    </p>
+                    <br/>
+                    <div className='icons'>
+                        <a href='https://github.com/kbuscemi' target='blank'><i className="fa fa-github github"></i></a>
+                        <a href='https://www.linkedin.com/in/kayleebuscemi/' target='blank'><i className="fa fa-linkedin-square linkedin"></i></a>
+                        <span className='email'>email: kaylee.buscemi@gmail.com</span>
                     </div>
                 </div>
               </div>
             </div>
-        </div>
+        // </div>
     )
 }
 
